86af91b9352c6f2d58c790729938342095cb5614
zzxwill <zzxwill@gmail.com>
2020-06-19 14:59:08 +0800

    move wordpress files to a folder

--- wordpress/wp-content/themes/inove/footer.php
+++ wordpress/wp-content/themes/inove/footer.php
@@ -34,6 +34,7 @@
 				bloginfo('name');
 			}
 		?>
+		京ICP备18010274号
 	</div>
 	<div id="themeinfo">
 		<?php printf(__('Theme by <a href="%1$s">NeoEase</a>. Valid <a href="%2$s">XHTML 1.1</a> and <a href="%3$s">CSS 3</a>.', 'inove'), 'http://www.neoease.com/', 'http://validator.w3.org/check?uri=referer', 'http://jigsaw.w3.org/css-validator/check/referer?profile=css3'); ?>
